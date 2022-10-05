00f2ed97ffb4e2e4108bc3f99561ae865e2bfbd7
Owain Williams <owilliams@spindogs.com>
2022-10-05 14:22:17 +0100

    [INITIAL] Setup and first version of stopwatch

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using StopWatchProgram;
+
+var stopwatch = new Stopwatch();
+Console.WriteLine("Type 'start' to start the stopwatch. Type 'quit' to close program");
+var input = Console.ReadLine();
+var watchStarted = false;
+
+
+while(input != null && input != "quit")
+{
+    if (input != null && input.ToLower() == "start")
+    {
+        if (!watchStarted)
+        {
+            Console.WriteLine("Stopwatch started. Type 'stop' to stop the stopwatch");
+            watchStarted = true;
+            Console.WriteLine("Started at: " + stopwatch.Start());
+            input = Console.ReadLine();
+
+        }
+        else
+        {
+            Console.WriteLine("Stopwatch has already been started");
+            input = Console.ReadLine(); 
+        }
+
+    }
+    if( input != null && input.ToLower() == "stop")
+    {
+        
+        Console.WriteLine();
+        Console.WriteLine("Stopwatch stopped");
+        watchStarted = false;
+        Console.WriteLine("Stopped at: " + stopwatch.Stop());
+        Console.WriteLine(stopwatch.ElapsedTime() + " has passed between starting and stopping the stopwatch");
+        input = Console.ReadLine();
+    }
+
+
+
+
+}
---
+++ StopWatch/StopWatch/Stopwatch.cs
@@ -0,0 +1,28 @@
+﻿namespace StopWatchProgram
+{
+    public class Stopwatch
+    {
+        private TimeOnly startedTime = new TimeOnly();
+        private TimeOnly stoppedTime = new TimeOnly();
+
+        public TimeOnly Start()
+        {
+            startedTime = TimeOnly.FromDateTime(DateTime.Now);
+            return startedTime;
+
+        }
+
+        public TimeOnly Stop()
+        {
+            stoppedTime = TimeOnly.FromDateTime(DateTime.Now);
+            return stoppedTime;
+            
+        }
+
+        public TimeSpan ElapsedTime()
+        {
+            var elapsedTime = stoppedTime - startedTime;
+            return elapsedTime;
+        }
+    }
+}
